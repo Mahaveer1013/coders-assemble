74d1bf52e21f92915bf416ae4c9c9e3d042cee0c
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-01-24 12:18:17 +0530

    nav design

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let menu_bar = document.querySelector('.menu-bar-div');
-let nav_list = document.querySelector('.nav-list');
-let menu_btn = document.querySelector('.menu-btn');
-menu_bar.addEventListener('click', () => {
+// let menu_bar = document.querySelectorAll('.side-menu');
+// let nav_list = document.querySelector('.nav-list');
+// let menu_btn = document.querySelector('.menu-btn');
+// let nav_li = document.querySelectorAll('.nav-li a');
+// menu_bar.forEach(element => {
+//     element.addEventListener('click', () => {
+//         nav_list.classList.toggle('active');
+//         menu_btn.classList.toggle('fa-bars');
+//         menu_btn.classList.toggle('fa-times');
+//         nav_li.forEach(nav_li => {
+//             nav_li.classList.toggle('side-menu');
+//         })
+//     })
+// });
+function open_menu() {
+    let menu_bar = document.querySelector('.menu-bar-div');
+    let nav_list = document.querySelector('.nav-list');
+    let menu_btn = document.querySelector('.menu-btn');
+
     nav_list.classList.toggle('active');
     menu_btn.classList.toggle('fa-bars');
     menu_btn.classList.toggle('fa-times');
-})
+}
