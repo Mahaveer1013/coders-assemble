985237d06bd31308bf2377fe115f36b362e75f1d
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-02-22 19:17:07 +0530

    events added

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function open_menu() {
-  let menu_bar = document.querySelector(".menu-bar-div");
   let nav_list = document.querySelector(".nav-list");
+  let nav_list_li = document.querySelectorAll('.nav-li')
   let menu_btn = document.querySelector(".menu-btn");
 
+  nav_list_li.forEach((li) => {
+    li.classList.toggle('active');
+  })
   nav_list.classList.toggle("active");
   menu_btn.classList.toggle("fa-bars");
   menu_btn.classList.toggle("fa-times");
@@ -13,7 +16,7 @@ let hours_div = document.querySelector(".hours");
 let minutes_div = document.querySelector(".minutes");
 let seconds_div = document.querySelector(".seconds");
 
-const targetDate = new Date(2024, 2, 30, 12, 0, 0); //  march 30- 2024- 12:00:00
+const targetDate = new Date(2024, 3 , 30, 12, 0, 0); //  march 30- 2024- 12:00:00
 
 function updateCountdown() {
   const currentDate = new Date();
@@ -26,9 +29,9 @@ function updateCountdown() {
   const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-  days_div.textContent = ` ${days} `;
-  hours_div.textContent = ` ${hours} `;
-  minutes_div.textContent = ` ${minutes} `;
-  seconds_div.textContent = ` ${seconds} `;
+  days_div.textContent = ` ${days} Days `;
+  hours_div.textContent = ` ${hours} Hrs `;
+  minutes_div.textContent = ` ${minutes} Mins`;
+  seconds_div.textContent = ` ${seconds} Secs`;
 }
 setInterval(updateCountdown, 1000);
